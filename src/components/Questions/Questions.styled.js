92067c578a5966fe69fef27425d90a02b129e884
IhorMarch <97352675+IhorMarch@users.noreply.github.com>
2024-01-28 14:37:07 +0200

    half

---
+++ src/components/Questions/Questions.styled.js
@@ -0,0 +1,66 @@
+
+
+import styled from 'styled-components';
+
+
+
+export const Section = styled.section`
+
+
+text-align: center;
+padding-top: 120px;
+margin-top:50px;
+padding-bottom: 120px;
+margin-left: auto;
+margin-right: auto;
+
+background-repeat: no-repeat;
+background-position: center;
+ background-size: cover;
+
+
+
+`;
+
+export const Title = styled.h2`
+font-weight: 500;
+font-size: 26px;
+line-height: 1.11;
+text-align: center;
+letter-spacing: 0.02em;
+text-transform: capitalize;            
+color: blue;
+/* margin-bottom: 72px; */
+
+margin-left: auto;
+margin-right: auto;
+
+`;
+
+export const Wrapper = styled.div`
+margin: 0 auto;
+/* height:432px;
+    width: 100%;
+    max-width: 428px; */
+    padding-left: 16px;
+    padding-right: 16px;
+
+`;
+
+export const Answer = styled.p`
+
+font-weight: 500;
+font-size: 16px;
+line-height: 1.5;
+letter-spacing: 0.04em;
+padding: 16px 32px;
+
+
+`;
+
+export const Img = styled.img`
+object-fit: cover;
+height:268px;
+border-radius: 14px;
+
+`;
